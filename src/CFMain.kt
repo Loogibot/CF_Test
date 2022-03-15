fbed991ced2a4ce403312eb6c0501367b43eefa1
Loogibot <luigi_mota89@hotmail.com>
2022-03-14 20:37:26 -0400

    cleaned up CFMain for text-based simpler game design

--- src/CFMain.kt
+++ src/CFMain.kt
@@ -1,17 +1,14 @@
-fun main(args: Array<String>) {
+fun main() {
+
+    println("""
+        Welcome to Chain Fighter!!!
+        $drawPlayerMove is your available move
+                
+    """.trimIndent())
 
 }
 
-val playerChoice = MoveAvailable(1)
-val playerOption = playerChoice.randomMove(1)
-
-val drawPlayerMove = when (playerOption.name) {
-    "kick" -> KICK
-    "punch" -> PUNCH
-    "dodge" -> DODGE
-    "grab" -> GRAB
-    else -> SHIELD
-}
+val playerChoice = randomMove("Player")
 
 data class Move(val name: String, val damage: Int, val firstAdv: String, val secondAdv: String) {
 }
@@ -22,16 +19,22 @@ val dodge = Move("dodge",0, "kick", "grab")
 val shield = Move("shield",5, "punch", "dodge")
 val punch = Move("punch",15, "grab", "dodge")
 
-class MoveAvailable(movePos: Int) {
+val drawPlayerMove = when (playerChoice.name) {
+    "kick" -> kick
+    "punch" -> punch
+    "dodge" -> dodge
+    "grab" -> grab
+    else -> shield
+}
+val allMoves = listOf(kick,grab,dodge,shield,punch)
 
-    val allMoves = listOf(kick,grab,dodge,shield,punch)
-
-    fun randomMove(movePos: Int): Move {
-        return when (movePos) {
-            1 -> allMoves.random()
-            2 -> allMoves.random()
-            else -> allMoves.random()
+fun randomMove(player: String?): Move {
+    return when (player) {
+        "Player" -> {
+            allMoves.random()
+        }
+        else -> {
+            allMoves.random()
         }
     }
-
 }
