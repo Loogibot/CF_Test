f1f6316610f0ef63cb9f03068957ff40139d2960
Loogibot <luigi_mota89@hotmail.com>
2022-05-11 13:40:01 -0400

    messed branch
    trying to recover the latest update to the one filer

--- CF TEST/src/CFMain.kt
+++ CF TEST/src/CFMain.kt
@@ -1,19 +1,93 @@
-fun main(args: Array<String>) {
+fun main() {
 
+    val playerChoice1 = Fighter(200, "pos1")
+    val playerChoice2 = Fighter(200, "pos2")
+    val opponentChoice = Fighter(200, "pos3")
+
+    println("""
+        
+        Welcome to CHAIN FIGHTER!!!
+        
+        An opponent appears before you. You each have 200 HP.
+        Type 'K' for Kick, 'P' for Punch, 'G' for Grab, 'D' for Dodge and 'S' for Shield.
+        Only two moves will be available at each turn. Choose wisely! 
+        Type start to begin!
+        
+    """.trimIndent())
+
+    val gameTurn = GameState()
+
+    val gameStart = readln()
+
+    while (gameStart == "start") {
+        gameTurn.turn = true
+
+        println("""
+            Your current HP is ${playerChoice1.currentHP()}, opponent current HP is ${opponentChoice.currentHP()}.
+            ${playerChoice1.drawMove()} and ${playerChoice2.drawMove()} are your available move.
+    """.trimIndent())
+
+        break
+    }
 }
 
-val playerChoice = MoveAvailable(1)
-val playerOption = playerChoice.randomMove(1)
-
-val drawPlayerMove = when (playerOption.name) {
-    "kick" -> KICK
-    "punch" -> PUNCH
-    "dodge" -> DODGE
-    "grab" -> GRAB
-    else -> SHIELD
+class Results {
 }
 
-data class Move(val name: String, val damage: Int, val firstAdv: String, val secondAdv: String) {
+class GameState(// controls the flow of the game via turns
+    turnStart: Boolean = false,
+    moveOpen: Boolean = false, // Make moves available to choose from
+    val moveRun: Boolean = false, // runs moves
+    val result: Results? = null )
+
+{
+    var turn = when (turnStart) {
+        moveOpen -> true
+        else -> {}
+    }
+}
+
+
+class Fighter(private val hp: Int, private val position: String) {
+    // implement game state
+    // val gameState = GameState()
+    // gameState.turn
+
+    /* this class NEEDS this method, otherwise an init block or secondary constructor is needed,
+    or you'll get expecting member declaration
+    */
+
+    private fun whoIsPlaying(position: String): Move {
+        return when (position) {
+            "pos1" -> randomMove("move1")
+            "pos2" -> randomMove("move2")
+            else -> randomMove("Opponent")
+        }
+    }
+
+    private val move = whoIsPlaying(position)
+
+    fun currentHP() = hp
+
+    fun drawMove(): String {
+        return when (move) {
+            kick -> kick.name
+            punch -> punch.name
+            dodge -> dodge.name
+            grab -> grab.name
+            else -> shield.name
+        }
+    }
+}
+
+/* Create moves to be selected, with built-in name, damage, first and second advantages
+    all of this should be good for now
+ */
+
+data class Move(val name: String,
+                val damage: Int,
+                val firstAdv: String,
+                val secondAdv: String) {
 }
 
 val kick = Move("kick",25, "punch", "shield")
@@ -22,16 +96,13 @@ val dodge = Move("dodge",0, "kick", "grab")
 val shield = Move("shield",5, "punch", "dodge")
 val punch = Move("punch",15, "grab", "dodge")
 
-class MoveAvailable(movePos: Int) {
+val allMoves = listOf(kick,grab,dodge,shield,punch)
 
-    val allMoves = listOf(kick,grab,dodge,shield,punch)
-
-    fun randomMove(movePos: Int): Move {
-        return when (movePos) {
-            1 -> allMoves.random()
-            2 -> allMoves.random()
-            else -> allMoves.random()
-        }
+// Randomly selects a move, might need to change to accommodate
+fun randomMove(player: String): Move {
+    return when (player) {
+        "move1" -> allMoves.random()
+        "move2" -> allMoves.random()
+        else -> allMoves.random()
     }
-
 }
